5b2571386a6540714aa1e65c83d4398066a57ce9
Deepti Kushwaha <deeptikushwaha140@gmail.com>
2023-09-23 22:44:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,11 @@ import {useRef, useEffect} from 'react';
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Switch,Routes,Route,Redirect,Link} from "react-router-dom";
 
-//comp
+//components
 import Login from "./comp/Login/Login";
-//import Credittable from "./comp/Credittable/Credittable";
-//import Navbar from './comp/Navbar/Navbar';
 import Geom from './comp/Geom';
 import Contactform from './comp/Contactform/Contactform';
 import { animateScroll } from 'react-scroll';
-//import {Routes, Route,Router, useNavigate} from 'react-router-dom';
-//import {BrowserRouter,Routes,Route} from "react-router-dom";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { move } from 'react-scroll';
@@ -79,21 +75,13 @@ componentDidMount() {
     fontFamily: 'Cabin Sketch, sans-serif',
     // Add other styling properties as needed
    };
-  //   const [isOpen, setIsOpen] = useState(true)
-
-  //  const move = () => {
-  //   setIsOpen(!isOpen)
-  // }
-   const handleDownload = () => {
-    // Create a blob with the content you want to download
-    const blob = new Blob(['Hello, World!'], { type: 'text/plain' });
-
-    // Specify the file name for the download
-    const fileName = 'example.txt';
-
-    // Use the saveAs function from FileSaver.js to trigger the download
-    saveAs(blob, fileName);
-  };
+  //  const handleDownload = () => {
+  //   const blob = new Blob(['Hello, World!'], { type: 'text/plain' });
+  
+  //   const fileName = 'table.txt';
+  
+  //   saveAs(blob, fileName);
+  // };
   return(
   <Router>
   <div className="main1"> 
@@ -219,133 +207,6 @@ export default App
 
 
 
-
-
-
-
-// import React,{Component} from 'react';
-// import SearchLocationInput from "./comp/GooglePlacesApi";
-//  import MapComponent from "./comp/Map";
-// //  //import Button from './comp/Button/Button';
-//  import { useRef, useState, useEffect } from 'react' ;
-//  import axios from 'axios';
-// // //import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
-
-// //import MapLocator from './comp/Map';
-// /*import MapComponent from './comp/Map';
-
-
-// const Geom = () => {
-//   const nearbyPlaces = [
-//     { name: 'Place 1', lat: 37.7750, lng: -122.4183 },
-//     { name: 'Place 2', lat: 37.7745, lng: -122.4208 },
-//     // Add more nearby places here
-//   ];
-  
-   
-//   return (
-//     <div className="App">
-//       <h1>Nearby Places Locator</h1>
-//        <h1>Current Location</h1>
-//       <p>Latitude:{currentLocation.latitude}</p>
-//       <p>Longitude:{currentLocation.longitude}</p>
-//       <p>City:{currentLocation.city}</p>
-//       <MapComponent
-//         containerElement={<div style={{ height: '400px', width: '100%' }} />}
-//         mapElement={<div style={{ height: '100%' }} />}
-//         places={nearbyPlaces}
-//         selectedLocation={selectedLocation}
-//       />
-//     </div>
-//   );
-// };
-
-// export default App;*/
-
-
-
-// /*export class App extends Component {
-//   render() {
-//     return (
-      
-//       <Map google={this.props.google} zoom={14} onClick={this.onMapClicked}>
-         
-//         <Marker onClick={this.onMarkerClick}
-//                 name={'Current location'} />
- 
-//         { <InfoWindow onClose={this.onInfoWindowClose} visible={this.state.showingInfoWindow}>
-//             { <div>
-//               <h1>{this.state.selectedPlace.name}</h1>
-//             </div> }
-//         </InfoWindow> }
-//       </Map>
-//     );
-//   }
-// }
-
-// //export default App;
-// export default GoogleApiWrapper({
-//   apiKey: ("AIzaSyAHScxjPfxl7F27RlricNhL03TCDEYrupE"),
-//   //LoadingContainer: LoadingContainer
-// })(App)*/
-
-
-// function App() {
-//   const nearbyPlaces = [
-//     { name: 'Place 1', lat: 37.7750, lng: -122.4183 },
-//     { name: 'Place 2', lat: 37.7745, lng: -122.4208 },
-//     // Add more nearby places here
-//   ];
-//   const [currentLocation, setCurrentLocation] = useState({});
-  
-//   useEffect(() => {
-//     getLocation()
-//   }, []);
-//   const getLocation = async() => {
-//     const location = await axios.get('https://ipapi.co/json');
-//     setCurrentLocation(location.data);
-//   }  
-//   var [selectedLocation, setSelectedLocation] = useState({
-//     lat: 28.7041,
-//     lng: 77.1025,
-//   });
-//   // const getLocationJS = () => {
-//   //   navigator.geolocation.getCurrentPosition((position)=>{
-//   //     const {latitude, longitude} = position.coords;
-//   //     setCurrentLocationJs({latitude, longitude});
-//   //   });
-//   // }
-//   return (
-//     <div style={{ height: "100vh", width: "100%" }}>
-      
-//       <div className='searchplaces'>
-
-//       </div>
-      
-//       <h1>Current Location</h1>
-//       <p>Latitude:{currentLocation.latitude}</p>
-//       <p>Longitude:{currentLocation.longitude}</p>
-//       <p>City:{currentLocation.city}</p>
-      
-//       <SearchLocationInput setSelectedLocation={setSelectedLocation} />
-      
-//       <MapComponent
-//         containerElement={<div style={{ height: '400px', width: '100%' }} />}
-//         mapElement={<div style={{ height: '100%' }} />}
-//         places={nearbyPlaces}
-//         selectedLocation={selectedLocation}
-//       />
-//     </div>
-//   );
-// }
- 
-// export default App
-
-
-
-
-
-      
 
 
 
